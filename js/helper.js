c86b7f85579d1cea1e99a448a359847eb42fa9f9
Freddy <fpolania@gmail.com>
2018-05-11 17:38:49 -0400

    Archivos correctos

---
+++ js/helper.js
@@ -0,0 +1,101 @@
+//Bio object
+const bio = {
+  "name" : "Freddy Polanía",
+  "role" : "Visual designer and front-end web developer",
+  "message": "Design and technology altogether",
+  "skills": ["Visual designer", "UX design", "Web design", "Logo and branding", "Typography"],
+  "biopic" : "mdq-man-200.jpg"
+};
+
+//Projects object
+const projects = {
+  "works": [
+    {
+      "title": "Banco del Libro website",
+      "dates": "May 2013",
+      "description": "Web design, html/css implementation.",
+      "url" : "http://galerias.bid-dimad.org/bid_14/?p=4191",
+      "images" : [
+        {
+          "src": "bdl-web1-200.jpg",
+          "href" : "bdl-web1-900.jpg"
+        },
+        {
+          "src": "bdl-web2-200.jpg",
+          "href" : "bdl-web2-900.jpg"
+        }
+      ]
+    },
+    {
+      "title": "Novanet website",
+      "dates": "Jun 2015",
+      "description": "Responsive web design, html/css implementation.",
+      "images" : [
+        {
+          "src": "nn-web1-200.jpg",
+          "href" : "nn-web1-900.jpg"
+        },
+        {
+          "src": "nn-web2-200.jpg",
+          "href" : "nn-web2-900.jpg"
+        },
+        {
+          "src": "nn-web3-200.jpg",
+          "href" : "nn-web3-900.jpg"
+        }
+      ]
+    },
+    {
+      "title": "Merlin Data Quality",
+      "dates": "Ago 2017",
+      "description": "Corporate manual and commercial stationery design.",
+      "images" : [
+        {
+          "src": "mdq-man1-200.jpg",
+          "href" : "mdq-man1-900.jpg"
+        },
+        {
+          "src": "mdq-man3-200.jpg",
+          "href" : "mdq-man3-900.jpg"
+        }
+      ]
+    },
+    {
+      "title": "Take on Bar",
+      "dates": "May 2016",
+      "description": "Logo design, commercial stationery, brochure, web page.",
+      "images" : [
+        {
+          "src": "tob-pap1-200.jpg",
+          "href" : "tob-pap1-900.jpg"
+        },
+        {
+          "src": "tob-pap2-200.jpg",
+          "href" : "tob-pap2-900.jpg"
+        }
+      ]
+    },
+    {
+      "title": "Novanet",
+      "dates": "Dic 2017",
+      "description": "Logo design.",
+      "images" : [
+        {
+          "src": "nn-log-200.jpg",
+          "href" : "nn-log-900.jpg"
+        }
+      ]
+    },
+    {
+      "title": "Escuela de natación Rivas",
+      "dates": "Oct 2017",
+      "description": "Logo design.",
+      "images" : [
+        {
+          "src": "edr-log-200.jpg",
+          "href" : "edr-log-900.jpg"
+        }
+      ]
+    }
+  ]
+};
