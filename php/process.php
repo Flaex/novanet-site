8ff84943261e7b6968f442993e65beaa53615b10
Freddy Polania <fpolania@gruponovanet.com.ve>
2018-06-11 23:50:38 -0400

    Factor file

--- php/process.php
+++ php/process.php
@@ -2,7 +2,7 @@
 if(!isset($_POST['submit']))
 {
 	//This page should not be accessed directly. Need to submit the form.
-	echo "Error, es necesario enviar el mensaje de contacto";
+	echo "error; you need to submit the form!";
 }
 $name = $_POST['name'];
 $visitor_email = $_POST['email'];
@@ -11,20 +11,20 @@ $message = $_POST['message'];
 //Validate first
 if(empty($name)||empty($visitor_email))
 {
-    echo "Los campos nombre y apaellido son requeridos";
+    echo "Name and email are mandatory!";
     exit;
 }
 
 if(IsInjected($visitor_email))
 {
-    echo "Ingrese uan dirección de email válida";
+    echo "Bad email value!";
     exit;
 }
 
-$email_from = 'contact-form@gruponovanet.com.ve';//<== update the email address
-$email_subject = "Mensaje gruponovanet.com.ve";
-$email_body = "Has recibido un mensaje de $name.\n"
-    "Mensaje: \n $message.";
+$email_from = 'fpolania@gmail.com';//<== update the email address
+$email_subject = "New Form submission";
+$email_body = "You have received a new message from the user $name.\n".
+    "Here is the message:\n $message.\n".
 
 $to = "fpolania@gmail.com";//<== update the email address
 $headers = "From: $email_from \r\n";
