60ae67479f4ecc97ffb0dd58e4eb811c28694146
Freddy <fpolania@gmail.com>
2018-07-03 16:58:56 -0400

    - Gulp configuration

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,18 @@
 var gulp = require('gulp');
-
-
-gulp.task('default', defaultTask);
-
-function defaultTask(done) {
-  console.log('Hi Mom');
-  done();
-}
-
 var browserSync = require('browser-sync').create();
- browserSync.init({
-     server: "./"
- });
- browserSync.stream();
+
+
+gulp.task('default', function() {
+  console.log('Hi Mom');
+
+  browserSync.init({
+      server: "./"
+  });
+  browserSync.stream();
+
+});
+
+gulp.task('watch', function(){
+  gulp.watch('css/*.css', '[default]')
+
+});
