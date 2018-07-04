29195e19bcc7a80e88b462c9c1761248917959b0
Freddy <fpolania@gmail.com>
2018-07-04 17:19:38 -0400

    - Added task to watch css/html changes and reload browser's tab. - Shortcut to to open project patch to console

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,16 @@
-var gulp = require('gulp');
+var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 
 
+// Static Server + watching scss/html files
 gulp.task('default', function() {
-  console.log('Hi Mom');
 
-  browserSync.init({
-      server: "./"
-  });
-  browserSync.stream();
+    browserSync.init({
+        server: "./"
+    });
 
+    gulp.watch("*.html").on('change', browserSync.reload);
+    gulp.watch("css/*.css").on('change', browserSync.reload);
 });
 
-gulp.task('watch', function(){
-  gulp.watch('css/*.css', '[default]')
-
-});
+// Compile sass into CSS & auto-inject into browsers
