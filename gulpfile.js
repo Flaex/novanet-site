dd6ffaa94a8d12fae12d3abdecdee22fe5b5dad8
Freddy Polania <fpolania@gruponovanet.com.ve>
2018-07-07 20:12:31 -0400

    - eslint integration

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,25 @@
-const gulp        = require('gulp');
+const {src, task, watch} = require('gulp');
 const browserSync = require('browser-sync').create();
+const eslint = require('gulp-eslint');
 
-// Static Server + watching scss/html files
-gulp.task('default', function() {
-
-    browserSync.init({
-        server: "./"
-    });
-
-    gulp.watch("*.html").on('change', browserSync.reload);
-    gulp.watch("css/*.css").on('change', browserSync.reload);
+task('default', () => {
+  browserSync.init({
+      server: "./"
+  });
+  watch("*.html").on('change', browserSync.reload);
+  watch("css/*.css").on('change', browserSync.reload);
+  watch("js/*.js").on('change', browserSync.reload);
 });
 
-// Compile sass into CSS & auto-inject into browsers
+task('lint', () => {
+    return src(['js/*.js'])
+        // eslint() attaches the lint output to the "eslint" property
+        // of the file object so it can be used by other modules.
+        .pipe(eslint())
+        // eslint.format() outputs the lint results to the console.
+        // Alternatively use eslint.formatEach() (see Docs).
+        .pipe(eslint.format())
+        // To have the process exit with an error code (1) on
+        // lint error, return the stream and pipe to failAfterError last.
+        .pipe(eslint.failAfterError());
+});
